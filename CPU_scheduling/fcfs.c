0269f28dfbcb06876e8dc6c3b6b9d31afea0bd12
shashank <shashank-sj>
2018-09-02 11:47:12 +0530

    fixed Priority

---
+++ CPU_scheduling/fcfs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define max 50
+struct process
+{
+    int burst_t, comp_t, arr_t, wait_t;
+} p[max];
+void main()
+{
+    int n, clock_t = 0, process = 0;
+    float avgw_t = 0.0, avgt_t = 0.0;
+    printf("Enter no of processes:");
+    scanf("%d", &n);
+    printf("Enter the Burst time and arrival time\n");
+    while (process++ < n)
+    {
+        printf("p%d\t", process);
+        scanf("%d%d", &p[process].burst_t, &p[process].arr_t);
+        p[process].wait_t = clock_t - p[process].arr_t;
+        clock_t += p[process].burst_t;
+        p[process].comp_t = clock_t;
+        avgw_t += p[process].wait_t;
+        avgt_t += p[process].comp_t - p[process].arr_t;
+    }
+    avgt_t /= n;
+    avgw_t /= n;
+    printf("Avg Waiting time = %.2f\nAvg Turnaround time =%.2f\n", avgw_t, avgt_t);
+}
---
+++ CPU_scheduling/priority.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#define max 50
+struct process
+{
+    int burst_t, comp_t, arr_t, flag, wait_t,priority;
+} p[max];
+//Lowest priority is first
+void print_order(int arr[], int n)
+{
+    printf(" order of the processes:");
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+}
+void main(){
+    int key, n, i,j, finish, process = 0, order[max], total_bt = 0, run = 0;
+    float avgt_t=0.0,avgw_t=0.0;
+    printf("Enter no of processes:");
+    scanf("%d", &n);
+    printf("Enter the priority ,Burst time and arrival time\n");
+    while (process++ < n)
+    {
+        printf("p%d\t", process);
+        scanf("%d%d%d",&p[process].priority, &p[process].burst_t, &p[process].arr_t);
+        total_bt += p[process].burst_t;
+        order[process] = process;
+        p[process].flag = 0;
+    }
+    print_order(order,n);
+    for (i = 2; i <= n; i++){   //sort based on arr_t
+        key = order[i];
+        j = i - 1;
+        while (j >= 1 && p[order[j]].arr_t > p[key].arr_t)
+        {
+            order[j + 1] = order[j];
+            j = j - 1;
+        }
+        order[j + 1] = key;
+    }
+    print_order(order,n);
+    process = 0;
+    while (run <= total_bt && process++ < n)
+    {
+        if (p[order[process]].arr_t < run && p[order[process]].flag == 0)
+        {
+            for (int i = process; i <= n && p[order[i]].arr_t < run; i++)
+            {
+                key = order[i];
+                j = i - 1;
+                while (j >= process && p[order[j]].priority > p[key].priority)
+                {
+                    order[j + 1] = order[j];
+                    j = j - 1;
+                }
+                order[j + 1] = key;
+            }
+        }
+        p[order[process]].flag = 1;
+        run += p[order[process]].burst_t;
+    }
+    run = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d\t", order[i]);
+        p[i].wait_t = run;
+        run += p[i].burst_t;
+        p[i].comp_t = run;
+        avgw_t += p[i].wait_t;
+        avgt_t += p[i].comp_t;
+    }
+    avgt_t /= n;
+    avgw_t /= n;
+    printf("\nAvg Waiting time = %.2f\nAvg Turnaround time =%.2f\n", avgw_t, avgt_t);
+}
