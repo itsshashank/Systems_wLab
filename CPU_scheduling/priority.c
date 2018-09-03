2abd8ed7bcf851647e58545143395856e48ab323
shashank <shashank.sj1@programmer.net>
2018-09-03 14:27:33 +0530

    fixed for all arrive at same time

--- CPU_scheduling/priority.c
+++ CPU_scheduling/priority.c
@@ -28,7 +28,7 @@ void main(){
         order[process] = process;
         p[process].flag = 0;
     }
-    print_order(order,n);
+    //print_order(order,n);
     for (i = 2; i <= n; i++){   //sort based on arr_t
         key = order[i];
         j = i - 1;
@@ -39,14 +39,14 @@ void main(){
         }
         order[j + 1] = key;
     }
-    print_order(order,n);
+    //print_order(order,n);
     process = 0;
     while (run <= total_bt && process++ < n)
     {
-        if (p[order[process]].arr_t < run && p[order[process]].flag == 0)
+        if (p[order[process]].arr_t <= run && p[order[process]].flag == 0)
         {
-            for (int i = process; i <= n && p[order[i]].arr_t < run; i++)
-            {
+            for (int i = process; i <= n && p[order[i]].arr_t <= run; i++) //sort based on priority
+            {   //print_order(order,n);
                 key = order[i];
                 j = i - 1;
                 while (j >= process && p[order[j]].priority > p[key].priority)
@@ -63,12 +63,12 @@ void main(){
     run = 0;
     for (int i = 1; i <= n; i++)
     {
-        printf("%d\t", order[i]);
-        p[i].wait_t = run;
-        run += p[i].burst_t;
-        p[i].comp_t = run;
-        avgw_t += p[i].wait_t;
-        avgt_t += p[i].comp_t;
+        //printf("%d\t", order[i]);
+        p[order[i]].wait_t = run;
+        run += p[order[i]].burst_t;
+        p[order[i]].comp_t = run;
+        avgw_t += p[order[i]].wait_t;
+        avgt_t += p[order[i]].comp_t - p[order[i]].arr_t; //TAT= Completion time-arrival time
     }
     avgt_t /= n;
     avgw_t /= n;
