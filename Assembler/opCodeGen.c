823391ce4078c1e41083262ed9fc4e919292d96c
shashank-sj <shashank-sj>
2018-11-04 16:44:00 +0530

    generate opcode table

---
+++ Assembler/opCodeGen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define max 10
+struct opcode
+{
+    char mnemonic[max];
+    char code[max/2];
+} o;
+void main()
+{
+    char fname[max];
+    printf("enter the file name to store the opcode table:");
+    scanf("%s",fname);
+    FILE *f;
+    f=fopen(fname,"w");
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%s %s",o.mnemonic,o.code);
+        fprintf(f,"%s \t %s \n",o.mnemonic,o.code);
+    }
+    fclose(f);
+}
