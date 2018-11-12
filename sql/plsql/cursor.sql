3c2d889e52e44e30aa2ac1fe43734a2c8861fe5f
shashank-sj <shashank-sj>
2018-11-12 13:48:50 +0530

    functions completed

--- sql/plsql/cursor.sql
+++ sql/plsql/cursor.sql
@@ -245,3 +245,24 @@ create or replace PROCEDURE display(xdno number)
             dbms_output.put_line(aRow.id || ' ' || aRow.name ||' '|| aRow.sal);
        end loop;
       end;
+
+//functions
+DECLARE
+      id plemp.id%type;
+      sal plemp.sal%type;
+      FUNCTION dissal(xid number)RETURN number
+      is
+      BEGIN
+       FOR aRow IN (SELECT id, Name,sal fROM plemp WHere id = xid)
+       LOop
+            dbms_output.put_line('NAME ' || 'sal ');
+            dbms_output.put_line( aRow.name ||' '|| aRow.sal);
+            RETURN aRow.sal;
+       end loop;
+      end;
+BEGIN
+      id :=&id;
+      sal:=dissal(id);
+      dbms_output.put_line( 'SAl=' || sal);
+END;
+/
