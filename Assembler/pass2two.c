0a47d70a72bac715b33ab2990fb004032af50afd
shashank-sj <shashank-sj>
2018-11-14 11:47:37 +0530

    completed 2nd pass and single pass

--- Assembler/pass2two.c
+++ Assembler/pass2two.c
@@ -22,11 +22,10 @@ char *itoa(long i, char *s, int dummy_radix)
     sprintf(s, "%ld", i);
     return s;
 }
-//
 int main()
 {
-    FILE *fin, *fsymt, *fop, *fout;
-    int addr, len;
+    FILE *fin, *fsymt, *fop, *fout, *fopgm;
+    int addr, len, startaddr, end;
     char filename[max];
     printf("Enter the intermediate file(output of pass 1) :");
     scanf("%s", filename);
@@ -41,67 +40,88 @@ int main()
     if (strcmp(in.opcode, "START") == 0)
     {
         fprintf(fout, "%s\t%s\t%s\n", in.label, in.opcode, in.operand);
+        startaddr = atoi(in.operand);
     }
     while (fscanf(fin, "%s %s %s %s", outcode, in.label, in.opcode, in.operand) != EOF)
     {
-        printf("\n%s\t%s\t%s\t%s", outcode, in.label, in.opcode, in.operand);
-        fprintf(fout,"%s\t%s\t%s\t%s\t", outcode, in.label, in.opcode, in.operand);
+        end = atoi(outcode);
+        fprintf(fout, "%s\t%s\t%s\t%s\t", outcode, in.label, in.opcode, in.operand);
         if (strcmp(in.opcode, "RESW") == 0 || strcmp(in.opcode, "RESB") == 0)
-            {
-                fprintf(fout,"\n");
-                continue;
-            }
+        {
+            fprintf(fout, "*\n");
+            continue;
+        }
         else if (strcmp(in.opcode, "WORD") == 0)
         {
             len = strlen(in.operand);
             itoa(atoi(in.operand), a, 10);
-            fprintf(fout,"00000%s\n", a);
+            fprintf(fout, "00000%s\n", a);
             continue;
-            //strcpy(outcode,"00000%s",a);
         }
         else if (strcmp(in.opcode, "BYTE") == 0)
+        {
+            len = strlen(in.operand);
+            int actual_len = len - 3;
+            for (int i = 2; i < (actual_len + 2); i++)
             {
-                //fprintf(fp1, "%d\t%s\t%s\t%s\t", address, label, opcode, operand);
-                len = strlen(in.operand);
-                int actual_len = len - 3;
-                for (int i = 2; i < (actual_len + 2); i++)
-                {
-                    itoa(in.operand[i], a, 16);
-                }
-                //fprintf(fp1, "\n");
-                //printf("code %s\n", a);
-                strcpy(outcode,a);
+                itoa(in.operand[i], a, 16);
             }
-
-            else
+            strcpy(outcode, a);
+        }
+        else
+        {
+            //search for the opcode
+            rewind(fop);
+            while (fscanf(fop, "%s %s", o.code, o.size) != EOF)
             {
-                //search for the opcode
-                rewind(fop);
-                while (fscanf(fop, "%s %s", o.code, o.size) != EOF)
+                if (strcmp(o.code, in.opcode) == 0)
                 {
-                    if (strcmp(o.code, in.opcode) == 0)
-                    {
-                        strcpy(outcode, o.size);
-                        break;
-                    }
+                    strcpy(outcode, o.size);
+                    break;
                 }
-                char temp[max];
-                //search symbol
-                rewind(fsymt);
-                while (fscanf(fsymt, "%s %s", s.symbol, temp) != EOF)
-                {
-                    //printf("comparing the symbol to symbol tabel %s with %s\n", in.label, s.symbol);
-                    if (strcmp(s.symbol, in.operand) == 0)
-                        break;
-                }
-                strcat(outcode, temp);
             }
-            fprintf(fout,"%s\n",outcode);
-            //printf("\ncode : %s", outcode);
+            char temp[max];
+            //search symbol
+            rewind(fsymt);
+            while (fscanf(fsymt, "%s %s", s.symbol, temp) != EOF)
+            {
+                if (strcmp(s.symbol, in.operand) == 0)
+                    break;
+            }
+            strcat(outcode, temp);
+        }
+        fprintf(fout, "%s\n", outcode);
     }
+    end -= startaddr ;
+    end++;
+    fclose(fout);
+    printf("Pass 2 Completed Sucessfully check finalout File\n");
+    //fclose(fopgm);
     fclose(fin);
     fclose(fsymt);
     fclose(fop);
+    fout = fopen("finalout.x","r");
+    fopgm = fopen("objprogram.dat","w");
+    fscanf(fout,"%s%s%s",in.label,in.opcode,in.operand);
+    if(strcmp(in.opcode,"START")==0)
+    {
+        itoa(end,outcode,16);
+        fprintf(fopgm,"H^%s^%s^%s\n",in.label,in.operand,outcode);
+    }
+    char imp[max];
+            fprintf(fopgm,"T^%s^%s",in.operand,outcode);
+    while(fscanf(fout,"%s%s%s%s%s",outcode,in.label,in.opcode,in.operand,imp)!=EOF)
+    {
+        if(strcmp(in.opcode,"WORD")==0 || strcmp(in.opcode,"BYTE")==0)
+        {
+            fprintf(fopgm,"\nT^%s^02^%s",outcode,imp);
+        }
+        else if(strcmp(imp,"*")!=0)
+            fprintf(fopgm,"^%s",imp);
+    }
+    itoa(startaddr,outcode,16);
+    fprintf(fopgm,"\nE^00%s\n",outcode);
     fclose(fout);
+    fclose(fopgm);
     return 0;
 }
